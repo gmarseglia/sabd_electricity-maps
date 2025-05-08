1ebb301141eefa0fb48dca322ed38ae10c308582
gmarseglia <g.marseglia.it@gmail.com>
2025-05-08 14:37:50 +0200

    Added scripts to download italy dataset

---
+++ utils/downloader.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+URL_FILE="datasets_url.txt"
+DATASET_DIR="../dataset"
+
+# Ensure the script gets the file as a parameter
+if [ -z "$URL_FILE" ]; then
+  echo "$URL_FILE does not exists."
+  exit 1
+fi
+
+# Ensure the $DATASET_DIR directory exists
+if [ ! -d "$DATASET_DIR" ]; then
+  echo "Directory $DATASET_DIR does not exist. Creating it..."
+  mkdir -p "$DATASET_DIR"
+fi
+
+# Read the file line by line
+while IFS= read -r line; do
+  # Skip lines that start with # (comments)
+  if [[ ! "$line" =~ ^# ]]; then
+    # Check if the line is not empty
+    if [[ -n "$line" ]]; then
+      echo "Downloading: $line"
+      wget -P "$DATASET_DIR" "$line"
+    fi
+  fi
+done < "$URL_FILE"
