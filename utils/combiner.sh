472ec07f396167d570bb99510c7ea89a7b7e0ecb
gmarseglia <g.marseglia.it@gmail.com>
2025-05-13 10:35:22 +0200

    Using now regional datasets

--- utils/combiner.sh
+++ utils/combiner.sh
@@ -16,38 +16,27 @@ if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
 fi
 
-# Loop through files containing 'monthly' in their name
-MONTHLY_DATASET="$OUTPUT_DIR/combined_monthly_dataset.csv"
-> "$MONTHLY_DATASET"
-for file in "$INPUT_DIR"/*monthly*; do
+# Loop through files containing 'IT' and 'hourly' in their name
+HOURLY_ITALY_DATASET="$OUTPUT_DIR/combined_dataset-italy_hourly.csv"
+> "$HOURLY_ITALY_DATASET"
+for file in "$INPUT_DIR"/*IT*hourly*; do
     # Check if the file exists and is a regular file
     if [ -f "$file" ]; then
         # Skip the first line and append the rest to the output file
-        tail -n +2 "$file" >> "$MONTHLY_DATASET"
+        tail -n +2 "$file" >> "$HOURLY_ITALY_DATASET"
     fi
 done
-echo "Combined content saved in $MONTHLY_DATASET"
 
-# Loop through files containing 'yearly' in their name
-YEARLY_DATASET="$OUTPUT_DIR/combined_yearly_dataset.csv"
-> "$YEARLY_DATASET"
-for file in "$INPUT_DIR"/*yearly*; do
+# Loop through files containing 'IT' and 'hourly' in their name
+HOURLY_SWEDEN_DATASET="$OUTPUT_DIR/combined_dataset-sweden_hourly.csv"
+> "$HOURLY_SWEDEN_DATASET"
+for file in "$INPUT_DIR"/*SE*hourly*; do
     # Check if the file exists and is a regular file
     if [ -f "$file" ]; then
         # Skip the first line and append the rest to the output file
-        tail -n +2 "$file" >> "$YEARLY_DATASET"
+        tail -n +2 "$file" >> "$HOURLY_SWEDEN_DATASET"
     fi
 done
-echo "Combined content saved in $YEARLY_DATASET"
 
-# Loop through files containing 'hourly' in their name
-HOURLY_DATASET="$OUTPUT_DIR/combined_hourly_dataset.csv"
-> "$HOURLY_DATASET"
-for file in "$INPUT_DIR"/*hourly*; do
-    # Check if the file exists and is a regular file
-    if [ -f "$file" ]; then
-        # Skip the first line and append the rest to the output file
-        tail -n +2 "$file" >> "$HOURLY_DATASET"
-    fi
-done
-echo "Combined content saved in $HOURLY_DATASET"
+
+echo "Combined dataset saved."
---
+++ utils/downloader.py
@@ -0,0 +1,19 @@
+regions = [
+    "IT-CNO", "IT-CSO", "IT-NO", "IT-SAR", "IT-SIC", "IT-SO",
+    "SE-SE1", "SE-SE2", "SE-SE3", "SE-SE4"
+]
+
+years = ["2021", "2022", "2023", "2024"]
+
+combinations = [
+    f"https://data.electricitymaps.com/2025-04-03/{s1}_{s2}_hourly.csv"
+    for s2 in years
+    for s1 in regions
+]
+
+print(combinations)
+
+with open("datasets_url.txt", "w") as file:
+    for combination in combinations:
+        file.write(f"{combination}\n")
+
