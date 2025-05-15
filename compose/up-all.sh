1fdd57e5bd4d406d7d41d38d544d33fce084de28
gmarseglia <g.marseglia.it@gmail.com>
2025-05-15 18:21:40 +0200

    Renamed compose scripts

--- compose/up-all.sh
+++ compose/up-all.sh
@@ -3,4 +3,4 @@ docker network create --driver bridge sabd-net
 docker-compose -f hdfs/docker-compose.yml up -d
 docker exec -it master hdfs namenode -format
 docker exec -it master /usr/local/hadoop/sbin/start-dfs.sh
-./run-spark.sh
+./up-spark.sh
